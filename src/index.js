621ff0d903f236de9a7eff404ee671fd8dae36a0
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-02-01 06:22:24 -0300

    create social media links function

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const socialMediaLinks = {
+  github: 'fabianoc.souzaa',
+  youtube: 'UCwkMamJh-BZavsvshnB_9ow',
+  facebook: 'fabianocardozosouza',
+  instagram: 'fabianoc.souzaa',
+  twitter: '/'
+}
+
+function changeSocialMediaLinks() {}
