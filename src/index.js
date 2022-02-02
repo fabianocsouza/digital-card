889d8818987df676b234901b6eaa6b134731aabc
Fabiano Cardozo de Souza <fabiano1403@hotmail.com>
2022-02-02 06:24:46 -0300

    using github api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const socialMediaLinks = {
-  github: 'fabianoc.souzaa',
-  youtube: 'UCwkMamJh-BZavsvshnB_9ow',
+  github: 'fabianocsouza',
+  youtube: '',
   facebook: 'fabianocardozosouza',
   instagram: 'fabianoc.souzaa',
-  twitter: '/'
+  twitter: ''
 }
 
-function changeSocialMediaLinks() {}
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      nameUser.textContent = data.name
+    })
+}
+
+getGithubProfileInfo()
